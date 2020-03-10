2b11b0bd0fdf5b5847c95e2478c0048be1824172
PatrickYoung03 <patrickyoung18@googlemail.com>
2020-03-10 13:40:04 +0000

    tue-afternoon

---
+++ models/index.js
@@ -0,0 +1,11 @@
+// require query from db
+
+// STEP 1 - CREATE FUNCTIONS TO POPULATE EACH INDIVIDUAL TABLE
+// provider POPULATE TABLE FUNCTION
+// destructure fields for provider table
+// make a variable called data, await query INSERT INTO providers, VALUES $1, $2 ETC...
+// match these too array with destructured variables
+// console.log(`success, you have added a .... to ....`)
+// REPEAT FOR OTHER 3 TABLES
+
+// STEP 2 -
